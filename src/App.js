63b4eaeebeda82b272248af904ef59d0baa9ba15
thiagoceron <thiagoceronalmeida@gmail.com>
2024-12-08 14:36:53 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './estilo.css';
 
 function App() {
+  const [nba, setNBA] = useState([]);
+
+  useEffect(() => {
+    fetch('/nba-team.json', {
+      headers: {
+        Accept: "application/json"  
+      }
+    })
+    .then((res) => res.json())
+    .then((res) => setNBA(res));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      <header>
+        <strong>React NBA</strong>
       </header>
+
+      {nba.map((item) => {
+        return (
+          <article key={item.location} className='location'>
+            <strong className='name'>{item.name}</strong>
+            <img src={item.logoUrl} className='logoUrl' alt={`${item.name} logo`} />
+            <p className='history'>{item.history}</p>
+          </article>
+        );
+      })}
     </div>
   );
 }
